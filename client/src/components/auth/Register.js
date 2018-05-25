1ddd7dde0e0771d2a44ddaf14d46ebdea22379c8
Guilherme Araujo <guiluis.aa@gmail.com>
2018-05-25 16:10:49 -0300

    Testing Registration With Our Form - No Redux Yet

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from "axios";
 
 export default class Register extends Component {
 
@@ -25,12 +26,14 @@ export default class Register extends Component {
 		
 		const newUser = {
 			name: this.state.name,
-			email: this.state.email,
+			email: this.state.email, 
 			password: this.state.password,
 			password2: this.state.password2
 		}
 
-		console.log(newUser);
+		axios.post('/api/users/register', newUser)
+			.then(res => console.log(res.data))
+			.catch(err => console.log(err.response.data));
 	}
 
 	render() {
